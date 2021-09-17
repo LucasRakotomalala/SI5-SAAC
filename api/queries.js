2e66070d7f5203063b44c90c203e0aea316b1c7e
Lucas Rakotomalala <lucas.rakotomalala@yahoo.fr>
2021-09-17 21:13:38 +0200

    Fix last commit

--- api/queries.js
+++ api/queries.js
@@ -4,11 +4,11 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  database: process.env.DATABASE_NAME || 'td_1',
-  user: process.env.DATABASE_USER || 'si5_sacc',
-  password: process.env.DATABASE_PASSWORD ||'dev_password',
-  host: process.env.DATABASE_URL || '127.0.0.1',
-  port: process.env.DATABASE_PORT || 5432,
+  database: process.env.DB_HOST || 'td_1',
+  user: process.env.DB_USER || 'si5_sacc',
+  password: process.env.DB_PASSWORD ||'dev_password',
+  host: process.env.DB_URL || '127.0.0.1',
+  port: process.env.DB_PORT || 5432,
 });
 
 export const incrementVisit = async (req, res) => {
