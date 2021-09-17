fe3af62811e1ea3ab40926b920366971ffddede6
Lucas Rakotomalala <lucas.rakotomalala@yahoo.fr>
2021-09-17 17:36:25 +0200

    Update Dockerfile & Add pg

--- api/app.js
+++ api/app.js
@@ -5,7 +5,6 @@ import { createServer } from 'http';
 const port = process.env.PORT || 3000;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('Hello World!');
   res.end();
